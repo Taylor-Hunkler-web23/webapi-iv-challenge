402a21e92d731ffc478ef8880e3e61614a45d423
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-31 15:11:07 -0400

    Changed message

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express =require('express');
 
-const server =express();
+const express = require('express');
+
+const server = express(); 
+
+server.get('/', (req, res) => {
+  res.status(200).json({ hello: 'Web from heroku' });
+});
 
 
-server.get('/', (req, res) =>{
-res.status(200).json({Hello: 'from heroku'});
-})
-
-
-const port =process.env.PORT || 4000;
+const port =process.env.PORT || 4000; 
 
 
 server.listen(4000, () => {
-    console.log(`\n Server running on port ${port} \n`);
-  });
-  
+  console.log(`\n Server running on port ${port} \n`);
+});
