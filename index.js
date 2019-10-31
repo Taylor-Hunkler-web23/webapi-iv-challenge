5af1a6973e6df750d6d12612753d4cc19517964f
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-31 16:13:17 -0400

    Changed port

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ server.get('/', (req, res) => {
 const port =process.env.PORT || 4000; 
 
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n Server running on port ${port} \n`);
 });
